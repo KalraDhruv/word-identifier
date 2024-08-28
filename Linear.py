fcfc0a7fe9e1b668ccd66891b22a63454a922f0c
KalraDhruv <dhruvkalra@proton.me>
2024-08-28 19:03:56 +0000

    Initialized Weights with bias

--- Linear.py
+++ Linear.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def xavier_initialization(n_in, n_out):
-    lower_limit = - np.sqrt(6/(n_in+n_out))
+def xavier_initialization(input_size, output_size):
+    lower_limit = - np.sqrt(6/(input_size+output_size))
     upper_limit = - lower_limit
-    return np.random.uniform(lower_limit, upper_limit, size=(n_in,n_out))
+    return np.random.uniform(lower_limit, upper_limit, size=(output_size,input_size))
 
 class Linear:
     def __init__(self,input_size:int, output_size:int, learning_rate:float):
@@ -12,6 +12,11 @@ class Linear:
         :param output_size: Number of outputs for the next layer
         :param learning_rate: Learning Rate for processing Gradient Descent
         """
+        weights = xavier_initialization(input_size, output_size)
+        # Adding bias after initializing the weights on the 0 th column
+        bias = np.zeros((output_size, 1))
+        self.weights = np.append(bias,weights, axis=1)
+        self.learning_rate = learning_rate
 
     def forward(self, x:np.ndarray)->np.ndarray:
 
